e228e462d70f5f977e3729887b89b7e79e9871aa
huangjunsen <951434130@qq.com>
2025-10-17 22:56:34 +0800

    chore: update application identifiers and titles to reflect new branding for EnvHub

--- src/main/index.ts
+++ src/main/index.ts
@@ -89,6 +89,7 @@ function createWindow(): void {
     width: 900,
     height: 670,
     show: false,
+    title: 'EnvHub',
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
@@ -120,8 +121,9 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.envhub')
 
+  app.setName('EnvHub')
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
